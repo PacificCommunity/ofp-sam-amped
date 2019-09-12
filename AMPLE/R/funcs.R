5ad04f02ac2a7eb4197c4a39a01b2dfa625faa1b
drfinlayscott <drfinlayscott@gmail.com>
2019-09-13 09:14:01 +1100

    Added SPC about function

--- AMPLE/R/funcs.R
+++ AMPLE/R/funcs.R
@@ -19,22 +19,24 @@ globalVariables(c("X20.", "X5.", "X50.", "X80.", "X95.", "bin", "ffmsymin", "ffm
 
 # This looks pretty crummy
 
-#' Maintainer and licence information.
+#' Maintainer, licence and SPC about information.
 #'
 #' Show the maintainer and licence for use in the AMPED PIMPLE applications.
+#' Also show the 'About SPC' information.
 #' 
 #' @return A shiny.tag for use in Shiny apps
 #' @rdname maintainer_and_licence
 #' @examples
 #' amped_maintainer_and_licence()
 #' pimple_maintainer_and_licence()
+#' spc_about()
 #' @export
 amped_maintainer_and_licence <- function(){
   out <- tags$html(
     tags$h1("AMPED"),
     tags$p("Amazing Management Procedures Exploration Device"),
     tags$footer(
-      tags$p("version 0.2.0 Drayton Manored"),
+      tags$p("version 0.3.0 My Jampandy"),
       tags$p("Copyright 2019 OFP SPC MSE Team."),
       tags$p("Distributed under the GPL 3"),
       tags$a("Soure code", href="https://github.com/PacificCommunity/ofp-sam-mse-popmodel-Shiny/tree/master/AMPED")
@@ -50,13 +52,27 @@ pimple_maintainer_and_licence <- function(){
     tags$h1("PIMPLE"),
     tags$p("Performance Indicators and Management Procedures Explorer"),
     tags$footer(
-      tags$p("version 0.1.0 Kebab Spider"),
+      tags$p("version 0.2.0 Rupert Trousers"),
       tags$p("Copyright 2019 OFP SPC MSE Team."),
       tags$p("Distributed under the GPL 3")
     )
   )
   return(out)
 }
+
+#' @rdname maintainer_and_licence
+#' @export
+spc_about <- function(){
+  out <- tags$html(
+    tags$p(style="opacity: 0.5", class="caption", align="center", HTML("&copy"), "Pacific Community, 2019"),
+    tags$h1("About us:"),
+    tags$p(align="justify", "The Pacific Community (SPC) is the principal scientific and technical organisation in the Pacific region, proudly supporting development since 1947. It is an international development organisation owned and governed by its 26 country and territory members. The members are: American Samoa, Australia, Cook Islands, Federated States of Micronesia, Fiji, France, French Polynesia, Guam, Kiribati, Marshall Islands, Nauru, New Caledonia, New Zealand, Niue, Northern Mariana Islands, Palau, Papua New Guinea, Pitcairn Islands, Samoa, Solomon Islands, Tokelau, Tonga, Tuvalu, United States of America, Vanuatu, and Wallis and Futuna."),
+    tags$p(align="justify", "In pursuit of sustainable development to benefit Pacific people, this unique organisation works across more than 25 sectors. SPC is renowned for its knowledge and innovation in such areas as fisheries science, public health surveillance, geoscience and conservation of plant genetic resources for food and agriculture."),
+    tags$p(align="justify", "Much of SPC's focus is on major cross-cutting issues, such as climate change, disaster risk management, food security, gender equality, human rights, non-communicable diseases and youth employment. Using a multi-sector approach in responding to its members' development priorities, SPC draws on skills and capabilities from around the region and internationally, and supports the empowerment of Pacific communities and sharing of expertise and skills between countries and territories."),
+    tags$p(align="justify", "With over 600 staff, SPC has its headquarters in Noumea, regional offices in Suva and Pohnpei, a country office in Honiara and field staff in other Pacific locations. Its working languages are English and French. See: ", a("https://www.spc.int", href="www.spc.int"))
+  )
+}
+
 
 # Make the empty reactive stock object
 # The stock object is not reactive, but the elements inside it are (it's like a list)
