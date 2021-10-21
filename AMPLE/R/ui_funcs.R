3ab472adcfbaf39022791936a0788a908e17e71a
Finlay Scott <drfinlayscott@gmail.com>
2021-10-22 09:07:04 +1100

    Renamed UI funcs script and function so used in all apps

--- AMPLE/R/ui_funcs.R
+++ AMPLE/R/ui_funcs.R
@@ -1,11 +1,17 @@
-# sidebar set func
+# All the sidebars have the same width and SPC logo
+# ui_funcs.R 
 
-# So all the sidebars have the same width and SPC logo
-#' Intro HCR sidebar setup
+# Author: Finlay Scott (SPC) <finlays@spc.int>
+# Sountrack: Disintegration Dubs by G36 vs JK Flesh
+# Distributed under the terms of the GNU General Public License GPL (>= 3)
+
+#' Sidebar setup for the apps
 #' 
 #' Internal function, not exported, to help maintain consistent sidebars in a tabbed app.
 #' @param ... Other stuff to add to the \code{sidebarPanel} function.
-intro_hcr_sidebar_setup <- function(...){
+#' @noRd
+#' @keywords internal
+sidebar_setup <- function(...){
   sb <- sidebarPanel(
             width=3,
             br(),
